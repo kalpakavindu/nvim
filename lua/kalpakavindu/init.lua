bf6dc707337c8152070867def69c080469873b21
kalpakavindu <kalpadevonline@gmail.com>
2024-07-29 02:07:09 +0530

    fix: overflowing diagostic virtual_text replaced with floating message box on cursor hover

--- lua/kalpakavindu/init.lua
+++ lua/kalpakavindu/init.lua
@@ -2,4 +2,22 @@ require("kalpakavindu.settings")
 require("kalpakavindu.lazy_init")
 require("kalpakavindu.remap")
 
+-- Set colortheme
 vim.cmd.colorscheme("tokyonight")
+
+-- Diagnostics on hover
+vim.diagnostic.config({
+  virtual_text=false, -- I don't want this
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic_cursor", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
+  end
+})
